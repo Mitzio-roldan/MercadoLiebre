560c74d7794698d964dc7db7aec7af57386f2b41
Mitzio-roldan <mitzioroldan124@gmail.com>
2021-09-20 21:04:49 -0300

    update

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ app.use(morgan('tiny'));
 app.use(express.static(path.resolve(__dirname, './public')))
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server running correctly')
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
 })
